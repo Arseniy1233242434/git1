45ba9dd642a1c679d5e14edc9a723612187a9011
user <senyakuzmin29@gmail.com>
2025-02-27 11:44:17 +0500

    first commit

---
+++ prgit/Form1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace prgit
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        int number;
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+        void generate()
+        {
+            Random a = new Random();
+            number = a.Next(1, 100);
+        }
+
+    }
+}
